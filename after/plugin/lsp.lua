86b4871634c03acc1718bee539ce76f5ba84a211
Valentin Terebenin <terebenin.work@gmail.com>
2024-01-02 01:04:47 +0300

    feat: before lazy

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -7,24 +7,13 @@ lsp.ensure_installed({
   'rust_analyzer',
 })
 
--- Fix Undefined global 'vim'
-lsp.configure('sumneko_lua', {
-    settings = {
-        Lua = {
-            diagnostics = {
-                globals = { 'vim' }
-            }
-        }
-    }
-})
-
 
 local cmp = require('cmp')
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
 local cmp_mappings = lsp.defaults.cmp_mappings({
   ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
   ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+  ['<cr>'] = cmp.mapping.confirm({ select = true }),
   ["<C-Space>"] = cmp.mapping.complete(),
 })
 
@@ -38,41 +27,22 @@ lsp.setup_nvim_cmp({
 })
 
 lsp.set_preferences({
-    suggest_lsp_servers = false,
-    sign_icons = {
-        error = 'E',
-        warn = 'W',
-        hint = 'H',
-        info = 'I'
-    }
+  suggest_lsp_servers = false,
+  sign_icons = {
+    error = 'E',
+    warn = 'W',
+    hint = 'H',
+    info = 'I'
+  }
 })
 
--- lsp.on_attach(function(client, bufnr)
---   local opts = {buffer = bufnr, remap = false}
--- 
---   if client.name == "eslint" then
---       vim.cmd.LspStop('eslint')
---       return
---   end
--- 
---   vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
---   vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
---   vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
---   vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
---   vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
---   vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
---   vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
---   vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
---   vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
---   vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
--- end)
 vim.keymap.set("n", "gd", vim.lsp.buf.definition)
 
 lsp.nvim_workspace()
 lsp.setup()
 
 vim.diagnostic.config({
-    virtual_text = true,
+  virtual_text = true,
 })
 
 -- LSP Mappings.
@@ -85,20 +55,30 @@ vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
 
 
 -- LSP Config for Godot
-require'lspconfig'.gdscript.setup{
+lspconfig = require'lspconfig'
+lspconfig.gdscript.setup{
   filetypes = { "gd", "gdscript", "gdscript3" },
 }
 
-require('lspconfig').rust_analyzer.setup {
-    settings = {
-        ['rust-analyzer'] = {
-            checkOnSave = {
-                allFeatures = true,
-                overrideCommand = {
-                    'cargo', 'clippy', '--workspace', '--message-format=json',
-                    '--all-targets', '--all-features'
-                }
-            }
+lspconfig.rust_analyzer.setup {
+  settings = {
+    ['rust-analyzer'] = {
+      checkOnSave = {
+        allFeatures = true,
+        overrideCommand = {
+          'cargo', 'clippy', '--workspace', '--message-format=json',
+          '--all-targets', '--all-features'
         }
+      }
     }
+  }
+}
+lspconfig.setup{
+  init_options = {
+    userLanguages = {
+      eelixir = "html-eex",
+      eruby = "erb",
+      rust = "html",
+    },
+  },
 }
