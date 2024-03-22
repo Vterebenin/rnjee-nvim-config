0f687dc9635347b3715b1a24c63e0ef3d4afc7ac
Valentin Terebenin <terebenin.work@gmail.com>
2024-03-22 21:32:02 +0300

    feat: add neotest & neotest-rust

---
+++ after/plugin/neotest.lua
@@ -0,0 +1,17 @@
+local wk = require("which-key")
+
+require("neotest").setup({
+  adapters = {
+    require("neotest-rust") {
+        args = { "--no-capture" },
+    }
+  }
+})
+wk.register({
+  ["<leader>t"] = { name = "Tests" },
+  ["<leader>tt"] = { function() require("neotest").run.run() end, "Test nearest" },
+  ["<leader>tc"] = { function() require("neotest").run.run(vim.fn.expand("%")) end, "Test current file" },
+  ["<leader>ts"] = { function() require("neotest").run.stop() end, "Test stop" },
+  ["<leader>ta"] = { function() require("neotest").run.attach() end, "Test attach?" },
+})
+
--- lua/ternow/lazy.lua
+++ lua/ternow/lazy.lua
@@ -135,4 +135,19 @@ require("lazy").setup({
   -- Debugging
   'nvim-lua/plenary.nvim',
   'mfussenegger/nvim-dap',
+
+  -- tests and shit
+  { "rouge8/neotest-rust" },
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-neotest/nvim-nio",
+      "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
+      "nvim-treesitter/nvim-treesitter"
+    },
+    adapters = {
+      "neotest-rust"
+    }
+  },
 })
