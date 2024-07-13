1b60ef7eea2d835139ee4a5d46aa3f5b5935173d
Valentin Terebenin <terebenin.work@gmail.com>
2024-07-14 01:06:39 +0300

    feat: update lsp for vue

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -5,9 +5,45 @@ local lsp_zero = require('lsp-zero')
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {},
+  ensure_installed = {
+    'tsserver',
+    'volar',
+  },
   handlers = {
-    lsp_zero.default_setup,
+    function(server_name)
+      require('lspconfig')[server_name].setup({})
+    end,
+    volar = function()
+      require('lspconfig').volar.setup({})
+    end,
+    tsserver = function()
+      local vue_typescript_plugin = require('mason-registry')
+        .get_package('vue-language-server')
+        :get_install_path()
+        .. '/node_modules/@vue/language-server'
+        .. '/node_modules/@vue/typescript-plugin'
+
+      require('lspconfig').tsserver.setup({
+        init_options = {
+          plugins = {
+            {
+              name = "@vue/typescript-plugin",
+              location = vue_typescript_plugin,
+              languages = {'javascript', 'typescript', 'vue'}
+            },
+          }
+        },
+        filetypes = {
+          'javascript',
+          'javascriptreact',
+          'javascript.jsx',
+          'typescript',
+          'typescriptreact',
+          'typescript.tsx',
+          'vue',
+        },
+      })
+    end,
   },
 })
 
