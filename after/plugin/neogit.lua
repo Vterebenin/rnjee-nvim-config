f6e844a457ea906986d192adc890c633d11e3481
Valentin Terebenin <terebenin.work@gmail.com>
2024-07-15 01:05:22 +0300

    feat: updates

--- after/plugin/neogit.lua
+++ after/plugin/neogit.lua
@@ -1,7 +1,9 @@
 local builtin = require('telescope.builtin')
 local wk = require("which-key")
+local neogit = require("neogit")
+
 wk.add({
   { "<leader>g", group = "Neogit" },
-  { "<leader>gg", ":Neogit<CR>", desc = "Open Neogit" },
+  { "<leader>gg", neogit.open(), desc = "Open Neogit" },
   { "<leader>gf", builtin.git_files, desc = "Git files" },
 })
--- lua/ternow/lazy.lua
+++ lua/ternow/lazy.lua
@@ -74,9 +74,12 @@ require("lazy").setup({
   {
     "NeogitOrg/neogit",
     dependencies = {
-      "nvim-lua/plenary.nvim",         -- required
-      "sindrets/diffview.nvim",        -- optional - Diff integration
+      "nvim-lua/plenary.nvim", -- required
+      "sindrets/diffview.nvim", -- optional - Diff integration
+
+      -- Only one of these is needed, not both.
       "nvim-telescope/telescope.nvim", -- optional
+      "ibhagwan/fzf-lua",            -- optional
     },
     opts = {}
   },
