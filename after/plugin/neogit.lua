c52a4f37e6ca53f887eca4d7c3a3c139b79c34a9
Valentin Terebenin <terebenin.work@gmail.com>
2024-07-15 01:10:36 +0300

    feat: update neovim

--- after/plugin/neogit.lua
+++ after/plugin/neogit.lua
@@ -1,9 +1,8 @@
 local builtin = require('telescope.builtin')
 local wk = require("which-key")
-local neogit = require("neogit")
 
 wk.add({
   { "<leader>g", group = "Neogit" },
-  { "<leader>gg", neogit.open(), desc = "Open Neogit" },
+  { "<leader>gg", ":Neogit<CR>", desc = "Open Neogit" },
   { "<leader>gf", builtin.git_files, desc = "Git files" },
 })
